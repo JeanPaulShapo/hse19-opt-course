e285d4deb3fb4bbd715ab35e7ad4d62a23c73a30
Nikita Shapovalov <shapovalovnikita@list.ru>
2019-02-16 01:55:27 +0300

    Add code for second practical task

---
+++ task2/optimization.py
@@ -0,0 +1,118 @@
+import numpy as np
+from collections import defaultdict, deque  # Use this for effective implementation of L-BFGS
+from utils import get_line_search_tool
+
+
+def nonlinear_conjugate_gradients(
+    oracle, x_0,
+    scheme='Polak-Ribiere',
+    restart_nu=None,
+    tolerance=1e-4, max_iter=500,
+    line_search_options={'method': 'Wolfe', 'c1': 1e-4, 'c2': 0.2},
+    display=False, trace=False
+):
+    """
+    Nonlinear conjugate gradient method for optimization (Polak--Ribiere version).
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func() and .grad() methods implemented for computing
+        function value and its gradient respectively.
+    x_0 : 1-dimensional np.array
+        Starting point of the algorithm
+    scheme: str
+        Name of scheme to use (available options: 'Fletcher-Reeves', 'Polak-Ribiere')
+    restart_nu: float or None
+        nu from Powell test to use in restart checks; pass None to disable restarts
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    display : bool
+        If True, debug information is displayed during optimization.
+        Printing format is up to the student and is not checked in any way.
+    trace:  bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+              the stopping criterion.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['grad_norm'] : list of values Euclidean norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    if scheme not in ('Fletcher-Reeves', 'Polak-Ribiere'):
+        raise ValueError('Unknown scheme: {!r}'.format(scheme))
+
+    # TODO: Implement nonlinear Conjugate Gradients method.
+    # Use line_search_tool.line_search() for adaptive step size.
+    return x_k, 'success', history
+
+
+def lbfgs(oracle, x_0, tolerance=1e-4, max_iter=500, memory_size=10,
+          line_search_options=None, display=False, trace=False):
+    """
+    Limited-memory Broyden–Fletcher–Goldfarb–Shanno's method for optimization.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func() and .grad() methods implemented for computing
+        function value and its gradient respectively.
+    x_0 : 1-dimensional np.array
+        Starting point of the algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    memory_size : int
+        The length of directions history in L-BFGS method.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    display : bool
+        If True, debug information is displayed during optimization.
+        Printing format is up to a student and is not checked in any way.
+    trace:  bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+              the stopping criterion.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    # TODO: Implement L-BFGS method.
+    # Use line_search_tool.line_search() for adaptive step size.
+    return x_k, 'success', history
